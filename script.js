5d7674dac360e398dea3a8cc002eb424da33b8e5
Sagar <sagark10038@gmail.com>
2026-09-05 14:26:14 +0530

    Add functions to manage produce data in localStorage

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+// ================================
+// AgriWaste - Main JavaScript File
+// ================================
 
+// Get all saved produce
+function getProduceData() {
+    const data = localStorage.getItem("agriWasteProduce");
+
+    if (data) {
+        return JSON.parse(data);
+    }
+
+    return [];
+}
+
+
+// Save produce data
+function saveProduceData(produce) {
+    const existingData = getProduceData();
+
+    existingData.push(produce);
+
+    localStorage.setItem(
+        "agriWasteProduce",
+        JSON.stringify(existingData)
+    );
+}
+
+
+// Clear all produce data
+function clearProduceData() {
+    localStorage.removeItem("agriWasteProduce");
+}
